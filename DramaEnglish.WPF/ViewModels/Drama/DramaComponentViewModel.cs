59f9844faeecf8a73da2bc20991106010cc2d858
HailongWu <13126506430@163.com>
2021-07-29 15:51:57 +0800

    2090729 单词mp4

--- DramaEnglish.WPF/ViewModels/Drama/DramaComponentViewModel.cs
+++ DramaEnglish.WPF/ViewModels/Drama/DramaComponentViewModel.cs
@@ -1,13 +1,11 @@
-﻿using DramaEnglish.WPF.ViewModels;
+﻿using CommonService.DB;
+using DramaEnglish.WPF.ViewModels;
 using Prism.Commands;
 using Prism.Events;
 using Prism.Regions;
 using Prism.Services.Dialogs;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -18,29 +16,23 @@ namespace DramaEnglish.UserInterface.ViewModels.Drama
 
         #region 属性字段
         private MediaElement MediaPlayer;
-        private string videoPath=$@"D:\GitHub\DramaEnglish\DramaEnglish.WPF\Words";
-        private string currentWord= "applause";
-        public string CurrentWord { get { return currentWord; } set { SetProperty(ref currentWord, value); } }
+        private WORD currentWord;
+        public WORD CurrentWord { get { return currentWord; } set { SetProperty(ref currentWord, value); } }
         private int Index = 0;
-        private List<string> words ;
-        private string currentWordDetail = "";
-        public string CurrentWordDetail { get { return currentWordDetail; } set { SetProperty(ref currentWordDetail, value); } }
-
-
-        private string currentWordLine = "";
-        public string CurrentWordLine { get { return currentWordLine; } set { SetProperty(ref currentWordLine, value); } }
+        private List<WORD> words;
         #endregion
 
         #region 构造函数
         public DramaComponentViewModel(IRegionManager regionManager, IDialogService dialogService, IEventAggregator ea)
           : base(regionManager, dialogService, ea)
         {
-            words = GetWords();
+            words = WordDBService.GetIDontKnowWORD();
         }
         #endregion
 
         #region 命令
-        public DelegateCommand<MediaElement> StartCommand => new((MediaPlayer) => {
+        public DelegateCommand<MediaElement> StartCommand => new((MediaPlayer) =>
+        {
             this.MediaPlayer = MediaPlayer;
             Play(CurrentWord);
             //foreach (var item in GetWords())
@@ -52,28 +44,50 @@ namespace DramaEnglish.UserInterface.ViewModels.Drama
 
         });
 
-        public DelegateCommand<Window> DragMoveCommand => new((Window) => {
+
+        public DelegateCommand<MediaElement> GetFreshCommand => new((MediaPlayer) =>
+        {
+            Index = 0;
+            words = WordDBService.GetFreshWrod();
+        });
+
+        public DelegateCommand<MediaElement> GetHardCommand => new((MediaPlayer) =>
+        {
+            Index = 0;
+            words = WordDBService.GetIDontKnowWORD();
+        });
+
+        public DelegateCommand<Window> DragMoveCommand => new((Window) =>
+        {
             Window.DragMove();
 
         });
-
-        
 
         public DelegateCommand<MediaElement> NextCommand => new((MediaPlayer) =>
         {
             this.MediaPlayer = MediaPlayer;
             CurrentWord = words[Index];
-            CurrentWordDetail = GetWordDetail(CurrentWord);
-            CurrentWordLine= GetWordLine(CurrentWord);
             Index++;
             Index = Index % words.Count;
             Play(CurrentWord);
+            WordDBService.WatcheWord(CurrentWord);
+        });
+
+        public DelegateCommand<MediaElement> IKnowCommand => new((MediaPlayer) =>
+        {
+            this.MediaPlayer = MediaPlayer;
+            CurrentWord = words[Index];
+            Index++;
+            Index = Index % words.Count;
+            Play(CurrentWord);
+            WordDBService.IKnowWord(CurrentWord);
         });
 
 
         public DelegateCommand<MediaElement> StopCommand => new((MediaPlayer) => { this.MediaPlayer = MediaPlayer; MediaPlayer.Stop(); });
 
-        public DelegateCommand<MediaElement> LoadingCommand => new((MediaPlayer) => {
+        public DelegateCommand<MediaElement> LoadingCommand => new((MediaPlayer) =>
+        {
             this.MediaPlayer = MediaPlayer;
         });
 
@@ -82,77 +96,13 @@ namespace DramaEnglish.UserInterface.ViewModels.Drama
 
         #region 函数方法
 
-        public void Play(string word) {
-            this.MediaPlayer.Source = new Uri($@"D:\GitHub\DramaEnglish\DramaEnglish.WPF\Words\{currentWord}\{currentWord}.ts");
+        public void Play(WORD word)
+        {
+            this.MediaPlayer.Source = new Uri($@"D:\GitHub\DramaEnglish\DramaEnglish.WPF\Words\{currentWord.EN}\{currentWord.EN}.ts");
 
             this.MediaPlayer.Play();
         }
-        
 
-            private string GetWordLine(string word)
-        {
-            try
-            {
-                using (StreamReader sr = new StreamReader($@"D:\GitHub\DramaEnglish\DramaEnglish.WPF\Words\{word}\台词.txt"))
-                {
-                    var sb = new StringBuilder();
-                    var line = string.Empty;
-                    while ((line=sr.ReadLine())!=null)
-                    {
-                        var RegexStr = $@"^\d.*\d$";  //匹配字符串的开始和结束是否为0-9的数字[定位字符]
-                        if (!Regex.IsMatch(line, RegexStr))
-                            sb.Append($@"{line}
-");
-                        if (line.ToLower().Contains(currentWord.ToLower()))
-                            sb.Append($@"============================================
-");
-                    }
-                    return sb.ToString();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
-            return word;
-        }
-        private string GetWordDetail(string word) {
-            try
-            {
-                using (StreamReader sr = new StreamReader($@"D:\GitHub\DramaEnglish\DramaEnglish.WPF\Words\{word}\{word}.txt"))
-                {
-                    return sr.ReadToEnd();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
-            return word;
-        }
-
-        public List<string> GetWords()
-        {
-            var words = new List<string>();
-            try
-            {
-                using (StreamReader sr = new StreamReader($@"D:\Github\DramaEnglish\DramaEnglish.WPF\Words\B_贝壳今日单词_纯单词.txt"))
-                {
-                    while (sr.ReadLine() != null)
-                    {
-                        words.Add(sr.ReadLine());
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(e.Message);
-            }
-            return words;
-        }
 
         #endregion
     }
